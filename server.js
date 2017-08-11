6427ab003081c1a33a88c3aa877878d65f419dcd
PRASANNADEVI259 <30697807+PRASANNADEVI259@users.noreply.github.com>
2017-08-11 20:38:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one',function(req,res){
+res.send('article one is requested and will be served here');
+});
+app.get('/article-two',function(req,res){
+    res.send('article two is requested here');
+});
+app.get('article-three',function(req,res){
+    res.send('article three is requested here');
+});
+
+}
+}
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
